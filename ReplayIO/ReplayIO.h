796dda55db8fe3f7fdc86a3b14a1e8acca88002e
Allen Wu <allen.wu@originate.com>
2014-03-28 11:46:56 -0700

    Initial Commit

---
+++ ReplayIO/ReplayIO.h
@@ -0,0 +1,13 @@
+//
+//  ReplayIO.h
+//  ReplayIO
+//
+//  Created by Allen Wu on 3/28/14.
+//  Copyright (c) 2014 Originate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReplayIO : NSObject
+
+@end
---
+++ ReplayIO/ReplayIO.m
@@ -0,0 +1,13 @@
+//
+//  ReplayIO.m
+//  ReplayIO
+//
+//  Created by Allen Wu on 3/28/14.
+//  Copyright (c) 2014 Originate. All rights reserved.
+//
+
+#import "ReplayIO.h"
+
+@implementation ReplayIO
+
+@end
